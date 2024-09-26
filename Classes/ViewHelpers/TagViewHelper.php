4eec6a4c995134d99bd10f8cc801e565214d80e7
Sven Wappler <wappler@wappler.systems>
2024-09-26 13:54:53 +0200

    add tag viewhelper

---
+++ Classes/ViewHelpers/TagViewHelper.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace wapplersystems\References\ViewHelpers;
+
+
+use FluidTYPO3\Vhs\Traits\TagViewHelperTrait;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use WapplerSystems\WsT3bootstrap\Fluid\ViewHelper\TagBuilder;
+
+/**
+ */
+class TagViewHelper extends AbstractViewHelper
+{
+    use TagViewHelperTrait;
+
+    /**
+     * @return void
+     */
+    public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('tags', 'mixed', 'The tags as ObjectStorage or array', true);
+        $this->registerArgument('classPrefix', 'string', '');
+    }
+
+    /**
+     * @return string
+     */
+    public function render()
+    {
+
+        $tags = $this->arguments['tags'];
+        $classPrefix = $this->arguments['classPrefix'] ?? '';
+
+        $tagString = '';
+        foreach ($tags as $tag) {
+            $tagString .= $classPrefix . $tag->getUid() . ' ';
+        }
+        return $tagString;
+    }
+}
